9808617e55a29726ca0396191d8a1e41fc5633a6
Clayton Brant <Claybrant@live.com>
2020-04-27 17:12:33 -0500

    Clean up
    Cleaned up old code that didn't need to be there from my flask template.

--- src/app.py
+++ src/app.py
@@ -1,4 +1,11 @@
 '''
+Clayton Brant (Remixt) 4/27/2020
+This file serves as the backend to the ValorStats Webapp for the Game Valorant(Riot Games)
+It connects to the riot games api and fetches game data (player and general game stats)
+example url:
+https://valorstats.com/api/v1/resources/riot?summoner=remixt&region=na1
+DEBUG MODE:
+http://127.0.0.1:5000/api/v1/resources/riot?summoner=remixt&region=na1
 '''
 import flask
 import simplejson as json
@@ -7,53 +14,38 @@ import requests
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
-config = {}
+
+riot_url = ".api.riotgames.com" #url for contacting the riot games api, requires region in front of it. example: na1.api.riotgames.com
+sum_url = "/lol/summoner/v4/summoners/by-name/" #url required for fetching data based on the summoner profile name
+config = {} #config loaded in via JSON file (config.json), for now this only contains the api key required for fetching data from riot
+
 with open('config.json') as json_file:
     config = json.load(json_file)
-print(config['api_key'])
 api_key = config['api_key']
-riot_url = ".api.riotgames.com" #url for contacting the riot games api, requires region in front of it. example: na1.api.riotgames.com
-sum_url = "/lol/summoner/v4/summoners/by-name/"
-def dict_factory(cursor, row):
-    d = {}
-    for idx, col in enumerate(cursor.description):
-        d[col[0]] = row[idx]
-    return d
 
 
-@app.route('/', methods=['GET'])
+
+@app.route('/', methods=['GET']) #default page
 def home():
     return '''<h1>ValorStats API</h1>'''
 
-
-@app.route('/api/v1/resources/pages/all', methods=['GET'])
-def api_all():
-    conn = sqlite3.connect('wikiscrape.db')
-    conn.row_factory = dict_factory
-    cur = conn.cursor()
-    all_pages = cur.execute('SELECT * FROM pages;').fetchall()
-    try:
-        for api_key in range(len(all_pages)):#format the inner json table
-            all_pages[api_key]['table'] = json.loads(all_pages[api_key]['table'])
-        return jsonify(all_pages)
-    except:
-        pass
-
-@app.errorhandler(404)
+@app.errorhandler(404) #errors are sent to 404 page
 def page_not_found(e):
     return "<h1>404</h1>", 404
 
-
+#valorant api isn't out yet, using league of legends summoner information as a placeholder
 @app.route('/api/v1/resources/riot', methods=['GET'])
 def api_filter():
+    #build the full api request url from the parameters in the front end request
     query_parameters = request.args
-
     sum_name = query_parameters.get('summoner')
     region = query_parameters.get('region')
+
     query = str("https://"+region+riot_url+sum_url+sum_name+"?api_key="+api_key)
-    print(query)
+
     results = requests.get(url = query) 
     results = json.loads(results.content)
+
     return jsonify(results)
+
 app.run()
-#app.run(host="0.0.0.0")
